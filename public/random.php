e6889b778adfac2e80210b6635e543e664c69730
virginie <virginiegodfrin@gmail.com>
2020-10-21 16:44:24 +0200

    serving index.html file

---
+++ public/random.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Your lucky number</title>
+    </head>
+    <body>
+        <h1>Your lucky number is: <?php echo random_int(1, 10); ?></h1>
+    </body>
+</html>
+
