449818e5a3b92181669a7e20045ad3e39af901d5
virginie <virginiegodfrin@gmail.com>
2020-10-21 19:40:56 +0200

    adding select element to the form

--- public/pictures.php
+++ public/pictures.php
@@ -1,8 +1,22 @@
 <?php
-    $nbdog = isset($_GET['number']) ? (int) $_GET['number'] : 1;
-    if ($nbdog < 1) {
-        $nbdog = 1;
+    $nbpictures = isset($_GET['number']) ? (int) $_GET['number'] : 1;
+    if ($nbpictures < 1) {
+        $nbpictures = 1;
     }
+    
+    $picture = isset($_GET['picture']) ? $_GET['picture'] : 'dog.jpg';
+    
+    $pictures = [
+        'puppy.jpg' => 'Puppy',
+        'apple.jpg' => 'Apple'
+    ];
+?>
+<?php
+    /*
+    * Derive the picture from the query parameter,
+    * but also provide a sensible default ('puppy.jpg'):
+    */
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,8 +31,19 @@
                         Select a picture:
                     </label>
                     <select name="picture" id="picture">
-                        <option value="dog.jpg">Dog</option>
-                        <option value="puppy.jpg">Puppy</option>
+                        <?php foreach ($pictures as $filename => $description) {
+                        ?>
+                            <option value="<?php
+                                echo htmlspecialchars($filename, ENT_QUOTES);
+                            ?>"<?php
+                                if (isset($_GET['picture']) && $_GET['picture'] === $filename) {
+                                    ?> selected<?php
+                                }
+                            ?>>
+                            <?php echo htmlspecialchars($description, ENT_QUOTES); ?>
+                            </option>
+                            <?php
+                        } ?>
                     </select>
                 </div>
                 <label for="number">
@@ -27,7 +52,7 @@
                 <input name="number" 
                        id="number"
                        value="<?php 
-                            if (isset($_GET['number'])) echo htmlspecialchars ($_GET['number']); 
+                            htmlspecialchars ($picture); 
                        ?>"
                        onclick="alert('Helloooo\o')"
                 >
@@ -38,11 +63,13 @@
         </form>
         <hr>
     <?php
-        for ($i = 1; $i <= $nbdog; $i++) {
+        for ($i = 1; $i <= $nbpictures; $i++) {
     ?>
     Cat 
     <?php echo $i; ?>:
-    <img src="img/dog.jpg" alt="dog <?php echo $i; ?>" width="10%">
+    <img src="img/<?php echo htmlspecialchars($picture, ENT_QUOTES)?>" 
+         alt="dog <?php echo $i; ?>" 
+         width="10%">
     <?php
         }
     ?>
