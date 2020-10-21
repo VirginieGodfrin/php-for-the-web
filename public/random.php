d244598d2380acb180bdc11db1c520ff1c0cd6ce
virginie <virginiegodfrin@gmail.com>
2020-10-21 17:14:32 +0200

    Building up a response

--- public/random.php
+++ public/random.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
+<?php $number = random_int(1, 20) ?>
 <html lang="en">
     <head>
         <title>Your lucky number</title>
     </head>
     <body>
-        <h1>Your lucky number is: <?php echo random_int(1, 10); ?></h1>
+        <h1>Your lucky number is: <?php echo $number ?></h1>
+        <?php if ($number > 5) { ?>
+            <h2>Nice!</h2>
+        <?php } ?>
     </body>
 </html>
 
