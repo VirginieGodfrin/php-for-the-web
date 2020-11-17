99d9b0c80776d8eb891c035182dbe3efe26e6dcc
virginie <virginiegodfrin@gmail.com>
2020-11-17 13:35:14 +0100

    CRUD - create - Adding a tour

--- pages/create-tour.php
+++ pages/create-tour.php
@@ -12,15 +12,47 @@
         $toursData = [];
     }
 
-    // Create a tour:
-    $toursData[] = [
-        'destination' => 'Berlin',
-        'number_of_tickets_available' => 10,
-        'is_accessible' => true
-    ];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $toursData[] = [
+            'destination' => isset($_POST['destination']) ? (string)$_POST['destination'] : '',
+            'number_of_tickets_available' => isset($_POST['number_of_tickets_available']) ? (int)$_POST['number_of_tickets_available'] : 0,
+            'is_accessible' => isset($_POST['is_accessible']) ? true : false
+        ];
+    }
     
     // Convert the tours data to JSON
     $jsonData = json_encode($toursData, JSON_PRETTY_PRINT);
     
     // Save the tours data to `data/tours.json`:
     file_put_contents($toursJsonFile, $jsonData);
+    
+    include(__DIR__ . '/../_header.php');
+?>
+
+<h1>Create a new tour</h1>
+    <form method="post">
+        <div>
+            <label for="destination">
+                Destination:
+            </label>
+            <input type="text" name="destination" id="destination">
+        </div>
+        <div>
+            <label for="number_of_tickets_available">
+                Number of tickets available:
+            </label>
+            <input type="number" name="number_of_tickets_available" id="number_of_tickets_available">
+        </div>
+        <div>
+            <label>
+                <input type="checkbox" name="is_accessible" value="yes">
+                Is accessible
+            </label>
+        </div>
+        <div>
+            <button type="submit">Save</button>
+        </div>
+    </form>
+
+<?php
+    include(__DIR__ . '/../_footer.php');
