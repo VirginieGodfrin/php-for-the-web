1c120e4310e6d3aae6b9afba5ac81bf23115908a
virginie <virginiegodfrin@gmail.com>
2020-11-18 17:18:03 +0100

    CRUD - create- showing submitted data

--- pages/create-tour.php
+++ pages/create-tour.php
@@ -15,29 +15,33 @@
     $formErrors = [];
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Step 1: normalize the request data:
-        $normalizedData[] = [
+        $normalizedData = [
             'destination' => isset($_POST['destination']) ? (string)$_POST['destination']:  "" ,
             'number_of_tickets_available' => isset($_POST['number_of_tickets_available']) ? (int)$_POST['number_of_tickets_available'] : 0,
             'is_accessible' => isset($_POST['is_accessible']) ? true : false
          ];
-        
         // Step 2: validate the normalized data
-        if ($normalizedData['destination'] == "") {
+        if ($normalizedData['destination'] === "") {
             $formErrors['destination'] = 'Please provide a destination';
         }
+        
         if ($normalizedData['number_of_tickets_available'] < 1) {
             $formErrors['number_of_tickets_available'] = 'Number of tickets available should be at least 1';
         }
         
-        if (count($formErrors) == 0) {
+        if (count($formErrors) === 0) {
             $toursData[] = $normalizedData;
             // Convert the tours data to JSON
             $jsonData = json_encode($toursData, JSON_PRETTY_PRINT);
             // Save the tours data to `data/tours.json`:
             file_put_contents($toursJsonFile, $jsonData);
+            
+            $_SESSION['message'] = 'The new tour was saved successfully';
+            header('Location: /create-tour');
+            exit;
         }
     }
-    
+
     include(__DIR__ . '/../_header.php');
 ?>
 <h1>Create a new tour</h1>
@@ -46,7 +50,9 @@
             <label for="destination">
                 Destination:
             </label>
-            <input type="text" name="destination" id="destination">
+            <input type="text" name="destination" id="destination" value="<?php
+                echo isset($normalizedData['destination']) ? htmlspecialchars($normalizedData['destination'], ENT_QUOTES) : '';
+            ?>">
             <?php
                 if (isset($formErrors['destination'])) {
             ?>
@@ -61,7 +67,9 @@
             <label for="number_of_tickets_available">
                 Number of tickets available:
             </label>
-            <input type="number" name="number_of_tickets_available" id="number_of_tickets_available">
+            <input type="number" name="number_of_tickets_available" id="number_of_tickets_available" value="<?php
+                echo isset($normalizedData['number_of_tickets_available']) ? htmlspecialchars( $normalizedData['number_of_tickets_available'], ENT_QUOTES ) : '';
+            ?>">
             <?php
                 if (isset($formErrors['number_of_tickets_available'])) {
             ?>
@@ -74,7 +82,13 @@
         </div>
         <div>
             <label>
-                <input type="checkbox" name="is_accessible" value="yes">
+                <input type="checkbox" name="is_accessible" value="yes"
+                <?php
+                    if (isset($normalizedData['is_accessible']) && $normalizedData['is_accessible']) {
+                ?> checked
+                <?php
+                    }
+                ?>>
                 Is accessible
             </label>
         </div>
