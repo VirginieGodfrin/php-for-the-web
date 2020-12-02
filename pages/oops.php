32547037b615fb2db766b2854939e53c1d4dbb98
virginie <virginiegodfrin@gmail.com>
2020-12-02 16:38:29 +0100

    Error handling - PHP Errors

--- pages/oops.php
+++ pages/oops.php
@@ -1,4 +1,6 @@
 <?php
-throw new RuntimeException('Something went wrong');
+fopen(__DIR__ . '/this-file-does-not-exist', 'r');
+echo 'PHP does not stop execution, even when it triggers an error';
+//throw new RuntimeException('Something went wrong');
 
 
